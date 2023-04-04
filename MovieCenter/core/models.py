5348bac12df994f8bbb3ce9380ef519d9e9cb792
Maximo Ballestrero <maximoballestrero@gmail.com>
2023-04-04 18:48:13 -0300

    cree el modelo review, migrue y registre en admin

--- MovieCenter/core/models.py
+++ MovieCenter/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Review(models.Model):
+    pelicula=models.CharField(max_length=40)
+    titulo=models.CharField(max_length=80)
+    texto=models.TextField()
+    fecha=models.DateField()
+    poster=models.ImageField()
+    autor=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} - {self.pelicula} - {self.titulo} - {self.poster} - {self.autor} - {self.fecha}'
