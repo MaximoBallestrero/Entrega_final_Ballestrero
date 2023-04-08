2687778441d1e2ff7e758e4dcf2c392b7221287a
Maximo Ballestrero <maximoballestrero@gmail.com>
2023-04-08 17:28:39 -0300

    agregue formulario para buscar reseñas, y retoques

--- MovieCenter/core/urls.py
+++ MovieCenter/core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import Index, LeerReview
+from core.views import Index, LeerReview, buscar_review, resultados_busqueda
 
 urlpatterns = [
     path('', Index.as_view(), name='Index'),
-    path('review/<pk>', LeerReview.as_view(), name='Leer-Review')
+    path('review/<pk>', LeerReview.as_view(), name='Leer-Review'),
+    path('buscar-review/', buscar_review, name='Buscar'),
+    path('resultado-busqueda/', resultados_busqueda, name='Resultado'),
 ]
--- MovieCenter/core/views.py
+++ MovieCenter/core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 #class-based views
 from django.views.generic import ListView
@@ -9,6 +10,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from core.models import Review
 
 # Create your views here.
+
+#vistas basadas en clases
 class Index(ListView):
     model=Review
     template_name='core/index.html'
@@ -16,3 +19,17 @@ class Index(ListView):
 class LeerReview(DetailView):
     model=Review
     template_name='core/leer_review.html'
+
+
+
+#vistas basadas en funciones
+def buscar_review(request):
+    return render(request, 'core/buscar_review.html')
+
+def resultados_busqueda(request):
+    if request.GET['pelicula']:
+        pelicula=request.GET['pelicula']
+        reviews=Review.objects.filter(pelicula__icontains=pelicula)
+        return render(request, 'core/resultado.html', {'reviews':reviews, 'pelicula':pelicula})
+    else:
+        return redirect('Buscar')
